356195d0fcdb763521d9296797b20ec22d8c11a6
Funnyseeker <samsonov.viacheslav@gmail.com>
2016-04-06 14:32:22 +0300

    restruct

---
+++ JDBCPractise/src/main/java/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MainFrame frame = new MainFrame();
+                frame.run();
+            }
+        });
+    }
+}
---
+++ JDBCPractise/src/main/java/application/ConnectionInfo.java
@@ -0,0 +1,50 @@
+package application;
+
+public class ConnectionInfo {
+    private String driver;
+    private String host;
+    private String port;
+    private String sid;
+    private String username;
+    private String password;
+
+    public ConnectionInfo() {
+    }
+
+    public ConnectionInfo(String driver, String host, String port, String sid, String username, String password) {
+        this.driver = driver;
+        this.host = host;
+        this.port = port;
+        this.sid = sid;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return "jdbc:" + driver + ":" + host + ":" + port + ":" + sid;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
